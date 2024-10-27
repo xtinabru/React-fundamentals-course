d27b18b30289f9f3aad829dbe5e61b7e8970cab3
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-10-27 03:38:23 +0300

    simple useState example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  
+  const[likes, setLikes] = useState(0)
+
+  function increment(){
+    setLikes(likes +1)
+  }
+
+  function decrement(){
+    setLikes(likes -1)
+  }
+
   return (
     <div>
-     
+    <h1>{likes}</h1> 
+     <button onClick={increment}>increment</button>
+     <button onClick={decrement}>Decrement</button>
 
     </div>
   );
