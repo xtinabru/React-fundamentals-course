a49376935ca4aa2787371ed9a4c3b58a11bdcb47
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-10-27 03:45:44 +0300

    simple onChange controlled  example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function App() {
   
   const[likes, setLikes] = useState(0)
+  const[value, setValue] = useState('text')
+
 
   function increment(){
     setLikes(likes +1)
@@ -15,6 +17,12 @@ function App() {
   return (
     <div>
     <h1>{likes}</h1> 
+    <h1>{value}</h1> 
+    <input 
+    type="text"
+    value ={value}
+    onChange={event => setValue(event.target.value)}
+    ></input>
      <button onClick={increment}>increment</button>
      <button onClick={decrement}>Decrement</button>
 
