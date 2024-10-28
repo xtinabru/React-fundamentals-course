53df1617d55a5f8028597fbb99b77a1ab65ab780
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-10-28 14:23:54 +0200

    sorting select

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'Description'},
   ])
 
+  const [selectedSort, setSelectedSort] = useState('')
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -20,9 +23,27 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+       <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sort"
+        options={[
+          {value: 'title', name: 'by title'},
+          {value: 'body', name: 'by description'}
+        ]}
+       />
+      </div>
       {posts.length !==0
       ?  <PostList remove={removePost} posts={posts} title={"Posts list 1"}/>
       : <h1 style={{textAlign: 'center'}}>No posts found</h1>
